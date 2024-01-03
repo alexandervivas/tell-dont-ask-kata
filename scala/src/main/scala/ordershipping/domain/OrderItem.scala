911fa214cd69b5795e9f14afcceeca604098ff01
aurelien <aurelien.dauchot@soprism.com>
2024-01-03 14:30:43 +0100

    Experimenting WIP

--- scala/src/main/scala/ordershipping/domain/OrderItem.scala
+++ scala/src/main/scala/ordershipping/domain/OrderItem.scala
@@ -5,4 +5,13 @@ class OrderItem(
     var quantity: Int,
     var taxedAmount: Double,
     var tax: Double
-)
+) {
+  val unitaryTax =
+    roundAt(2)((p.price / 100) * p.category.taxPercentage)
+  val unitaryTaxedAmount = roundAt(2)(p.price + unitaryTax)
+  val taxedAmount =
+    roundAt(2)(unitaryTaxedAmount * itemRequest.quantity)
+  val taxAmount = roundAt(2)(unitaryTax * itemRequest.quantity)
+
+
+}
