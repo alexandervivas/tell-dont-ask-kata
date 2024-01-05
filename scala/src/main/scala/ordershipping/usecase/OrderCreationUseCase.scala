93c5b506f5f93ba64b87b6b3e1b0b8a1f5736185
pslaulhe <pslaulhe@gmail.com>
2024-01-05 14:10:18 +0100

    change order creation to scala way

--- scala/src/main/scala/ordershipping/usecase/OrderCreationUseCase.scala
+++ scala/src/main/scala/ordershipping/usecase/OrderCreationUseCase.scala
@@ -12,42 +12,22 @@ class OrderCreationUseCase(
     val productCatalog: ProductCatalog
 ) {
   def run(request: SellItemsRequest): Unit = {
-    val order = new Order(
-      currency = "EUR",
-      status = OrderStatus.Created,
-      id = 1
-    )
 
-    for (itemRequest <- request.requests) {
+    val orderItems = request.requests.flatMap {  itemRequest =>
       val product = productCatalog.getByName(itemRequest.productName)
-
       if (product.isEmpty)
         throw new UnknownProductException
       else {
-        val orderItems = product.map(p => OrderItem(p, itemRequest.quantity))
-
-
-        product.foreach(p => {
-//          val unitaryTax =
-//            roundAt(2)((p.price / 100) * p.category.taxPercentage)
-//          val unitaryTaxedAmount = roundAt(2)(p.price + unitaryTax)
-//          val taxedAmount =
-//            roundAt(2)(unitaryTaxedAmount * itemRequest.quantity)
-//          val taxAmount = roundAt(2)(unitaryTax * itemRequest.quantity)
-
-          val orderItem = new OrderItem(
-            product = p,
-            quantity = itemRequest.quantity
-          )
-         //order.addItem(orderItem)
-        })
+        product.map(p => OrderItem(p, itemRequest.quantity))
       }
     }
-    orderRepository.save(order)
-  }
 
-  private def roundAt(p: Int)(n: Double): Double = {
-    val s = math pow (10, p)
-    (math round n * s) / s
+    val order = Order(
+      currency = "EUR",
+      status = OrderStatus.Created,
+      id = 1,
+      orderItems = orderItems
+    )
+    orderRepository.save(order)
   }
 }
